3a376e0881c479de91984094ccc21e5fd38965fe
DavidTorrejano <Davitorrejano@gmail.com>
2023-05-02 16:38:57 -0500

    Este es mi segundo commit

---
+++ OtraClase.java
@@ -0,0 +1,16 @@
+public class OtraClase {
+
+    boolean piensa;
+
+    OtraClase(boolean piensa){
+        this.piensa= piensa;
+    }
+
+    public boolean isPiensa() {
+        return piensa;
+    }
+
+    public void setPiensa(boolean piensa) {
+        this.piensa = piensa;
+    }
+}
