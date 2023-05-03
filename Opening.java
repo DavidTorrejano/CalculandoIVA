12164ce08f9920ffe58bd09177377f1926b51e6e
DavidTorrejano <Davitorrejano@gmail.com>
2023-05-02 22:58:52 -0500

    Acto2

---
+++ Opening.java
@@ -0,0 +1,21 @@
+public class Opening {
+
+    private String pais;
+    private int ciudades;
+
+    public String getPais() {
+        return pais;
+    }
+
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+
+    public int getCiudades() {
+        return ciudades;
+    }
+
+    public void setCiudades(int ciudades) {
+        this.ciudades = ciudades;
+    }
+}
